158e7d6f742b6a51782c803be3a0caa16794ef18
Matias Nazabal <71480431+MatiasNaz@users.noreply.github.com>
2026-09-01 12:01:04 -0400

    fixed header styling and spacing for menu page

--- src/sections/SignatureMenu/SignatureMenu.tsx
+++ src/sections/SignatureMenu/SignatureMenu.tsx
@@ -9,7 +9,16 @@ function SignatureMenu() {
       <section id="signature-menu" className="signature-menu__section">
         <main>
           <div className="menu__container">
-            <div className="menu__content"></div>
+            <div className="menu__content">
+              <div className="menu__header-container">
+                <div className="menu__eyebrow">our signature menu</div>
+                <div className="menu__header">
+                  <h4>
+                    Cocktails made with house-made syrups and fresh ingredients.
+                  </h4>
+                </div>
+              </div>
+            </div>
           </div>
         </main>
       </section>
